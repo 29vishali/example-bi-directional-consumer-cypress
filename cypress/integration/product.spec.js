69f993255b3c012d75ae0fcfc8c56ece9f794847
29visa <visa.govtech@gmail.com>
2023-04-06 23:25:16 +0800

    supplyally

--- cypress/integration/product.spec.js
+++ cypress/integration/product.spec.js
@@ -5,22 +5,31 @@ describe('product page', () => {
     cy.intercept(
       {
         method: 'GET',
-        url: '**/product/*'
+        url: '**/userLocationRoles'
       },
       {
         statusCode: 200,
         body: { ...productResponse },
-        headers: { 'access-control-allow-origin': '*' }
+        headers: {
+          'access-control-allow-origin': '*',
+          Cookie:
+            'token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4OTYwMzNhZC0yYzcwLTRkNmMtOTY1Ny1mZWVlOWE0OGQxOTUiLCJzZXNzaW9uSWQiOiI4MjhjMDkyOC0xNTE1LTQyZWQtYmJkOS1jM2Q4OTBhOTBlMDkiLCJleHAiOjE2ODA3OTUyNjUsImlhdCI6MTY4MDc5MTY2NX0.diE_CJjg1aI0g7CNlLNc-9Q-J0Bqj5CtC0hB5JYIC1WI4Ooi5aE06OsQaEy2ChxF_HeNH2dYSDvMJtREfkJV3Q'
+        }
       }
     ).as('getProduct')
     cy.setupPact(Cypress.env('PACT_CONSUMER'), Cypress.env('PACT_PROVIDER'))
-    cy.visit('http://localhost:3000/products/09')
+
+    const auth =  {
+        username:"sally",
+        password:"ilovesally<3",
+      };
+  cy.visit(url, { auth });
+    cy.visit('https://www.admin.stg.supplyally.gov.sg/')
   })
 
   it('displays product item', () => {
-    cy.get('.product-id').contains('09')
-    cy.get('.product-name').contains('Gem Visa')
-    cy.get('.product-type').contains('CREDIT_CARD')
+    cy.get(`[data-cy="mock-sp-login-button"]`).click();
+    cy.get(`[data-cy=cfp-campaign-card]`).first().should("be.visible"); // should validate the stubbed response
   })
 
   after(() => {
